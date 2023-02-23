65447aa7e06f5b81b54589c84934848f04b4a614
rnamocot <namocotrenie@gmail.comgit config --global user.name>
2023-02-23 21:12:33 +0800

    fonts changes

--- index.php
+++ index.php
@@ -25,7 +25,6 @@ if (isset($_SESSION['username'])) {
     <!--googlefont-->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Inter;:wght@200;300;700;800&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
 
     <!-- Add icon library -->
