cb6e642aeb0ce79ad5eb09b3f269fe3d02b86388
rnamocot <namocotrenie@gmail.comgit config --global user.name>
2023-02-26 21:01:56 +0800

    logout

--- logout.php
+++ logout.php
@@ -1,12 +1,19 @@
 <?php
+session_start();
+session_unset();
+session_destroy();
+header("Location: index.php");
+exit;
+
+
 // logout.php
-if (isset($_SESSION['username'])) {
-    // unset the session
-    session_unset();
-    // destroy the session
-    session_destroy();
-    // redirect the user to the login page
-    header('Location: index.php');
-    exit();
-}
+// if (isset($_SESSION['username'])) {
+//     // unset the session
+//     session_unset();
+//     // destroy the session
+//     session_destroy();
+//     // redirect the user to the login page
+//     header('Location: index.php');
+//     exit();
+// }
 ?>
